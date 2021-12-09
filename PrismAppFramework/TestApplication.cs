fca25ec51557222578ea2485c236aae5951db249
Georgia Nelson <georgia.e.nelson@gmail.com>
2021-12-08 22:25:52 -0600

    Add project files.

---
+++ PrismAppFramework/TestApplication.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace PrismAppFramework
+{
+    public abstract class TestApplication : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            ContainerRegistry = containerRegistry;
+            RegisterApplicationSpecificTypes(containerRegistry);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HandlePostInitialize();
+        }
+
+        protected override IContainerExtension CreateContainerExtension() => new UnityContainerExtension();
+
+        public IContainerRegistry? ContainerRegistry { get; private set; }
+
+        // New methods
+        protected abstract void RegisterApplicationSpecificTypes(IContainerRegistry containerRegistry);
+        protected abstract void HandlePostInitialize();
+    }
+}
---
+++ PrismTest/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using PrismAppFramework;
+using System;
+using System.Windows;
+
+namespace PrismTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void RegisterApplicationSpecificTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<Test>();
+        }
+
+        protected override void HandlePostInitialize()
+        {
+            Test test = Container.Resolve<Test>();
+            MainWindow.Title = test.GetHeader();
+        }
+    }
+}
